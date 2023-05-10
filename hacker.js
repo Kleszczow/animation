97c701a1fee5c1fbf00f11c6f7174dfaef0da69e
Tymek Slomski <tymekslomski@gmail.com>
2023-05-10 22:08:58 +0200

    new text animation - hacked text

--- hacker.js
+++ hacker.js
@@ -1,12 +1,9 @@
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
 let interval = null;
 
 document.querySelector("h1").onmouseover = (event) => {
   let iteration = 0;
-
   clearInterval(interval);
-
   interval = setInterval(() => {
     event.target.innerText = event.target.innerText
       .split("")
@@ -14,15 +11,12 @@ document.querySelector("h1").onmouseover = (event) => {
         if (index < iteration) {
           return event.target.dataset.value[index];
         }
-
         return letters[Math.floor(Math.random() * 26)];
       })
       .join("");
-
     if (iteration >= event.target.dataset.value.length) {
       clearInterval(interval);
     }
-
     iteration += 1 / 3;
   }, 80);
 };
